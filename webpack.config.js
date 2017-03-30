d64d713129ce00b6ef301417cf688dc8a340b65a
Mike Kornelson <mike@thrconsulting.ca>
2017-03-30 11:20:08 -0500

    * fixed package.json * remove webpack css-loader stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 var nodeExternals = require('webpack-node-externals');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var cssModulesValues = require('postcss-modules-values');
 
 const root = path.resolve(__dirname);
 
@@ -34,47 +32,13 @@ module.exports = {
 					}
 				],
 			},
-			{
-				test: /.css$/,
-				exclude: /node_modules/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					loader: [
-						{
-							loader: 'css-loader',
-							query: {
-								modules:true,
-								importLoaders: 1,
-								localIdentName: '[name]_[local]_[hash:base64:5]'
-							}
-						},
-						{
-							loader: 'postcss-loader'
-						}
-					],
-				}),
-			}
 		]
 	},
 	plugins: [
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				postcss: [cssModulesValues]
-			}
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
 		}),
-		new ExtractTextPlugin('[name].css'),
-	// 	new webpack.optimize.UglifyJsPlugin({
-	// 		compress: {
-	// 			warnings: false,
-	// 		},
-	// 	}),
 	],
-	resolve: {
-		alias: {
-			'inputmask.dependencyLib': path.join(root, 'node_modules/jquery.inputmask/dist/inputmask/inputmask.dependencyLib.js'),
-			'inputmask.extensions': path.join(root, 'node_modules/jquery.inputmask/dist/inputmask/inputmask.extensions.js'),
-			'inputmask.numeric.extensions': path.join(root, 'node_modules/jquery.inputmask/dist/inputmask/inputmask.numeric.extensions.js'),
-			'inputmask': path.join(root, 'node_modules/jquery.inputmask/dist/inputmask/inputmask.js')
-		},
-	},
 };
